ff2ac3792947715f214b4655670673151eafec2a
Paulo Ricardo de Carvalho Magalhães <2017118050287@ifpi.int>
2019-06-17 08:33:22 -0300

    Primeira classe de dominio

--- src/main/java/com/ifpi/ted2019/resources/CategoriaResource.java
+++ src/main/java/com/ifpi/ted2019/resources/CategoriaResource.java
@@ -1,16 +1,24 @@
 package com.ifpi.ted2019.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ifpi.ted2019.domain.Categorias;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-		public String listar() {
-		return "Rest funcionando";
+		public List <Categorias> listar(){
+		List <Categorias> categorias= new ArrayList<Categorias>();
+		categorias.add(new Categorias(1, "Informática"));
+		categorias.add(new Categorias(2, "Escritório"));
+		return categorias;
 	}
 
 }
